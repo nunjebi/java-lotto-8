f060f1ffdfc580793ed07f702bfae2601607de39
nunjebi <nunjebi6535@gmail.com>
2025-11-01 23:39:46 +0900

    feat: 로또 구입 금액 입력 기능 구현
    - 구입 금액 입력 기능 추가
    - LottoConstants.java에 상수값 분리
    - README.md의 기능 목록에 '로또 구입 금액 입력 받기' 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,49 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int inputPurchaseAmount = inputLottoPurchaseAmount();
+
+        System.out.println("money: " + inputPurchaseAmount); // print test
+    }
+
+    private static int inputLottoPurchaseAmount() {
+        while (LottoConstants.OCCURRENCE_ERROR) {
+            try {
+                printInputPurchaseAmountGuide();
+                String inputPurchaseAmount = Console.readLine();
+
+                validateInputPurchaseAmount(inputPurchaseAmount);
+
+                return Integer.parseInt(inputPurchaseAmount);
+            } catch (IllegalArgumentException exception) {
+                System.out.println(exception.getMessage());
+            }
+        }
+    }
+
+    private static void printInputPurchaseAmountGuide() {
+        System.out.println(LottoConstants.INPUT_PURCHASE_AMOUNT_GUIDE_MESSAGE);
+    }
+
+    private static void validateInputPurchaseAmount(String inputPurchaseAmount) {
+        if (isNotPositiveInteger(inputPurchaseAmount))
+            throw new IllegalArgumentException(LottoConstants.NOT_POSITIVE_INTEGER_ERROR_MESSAGE);
+
+        int purchaseAmount = Integer.parseInt(inputPurchaseAmount);
+        if (isNotMultipleOfUnit(purchaseAmount))
+            throw new IllegalArgumentException(LottoConstants.MULTIPLE_OF_UNIT_ERROR_MESSAGE);
+    }
+
+    private static boolean isNotPositiveInteger(String inputPurchaseAmount) {
+        return !inputPurchaseAmount.matches("^[1-9]\\d*$");
+    }
+
+    private static boolean isNotMultipleOfUnit(int inputPurchaseAmount) {
+        if (inputPurchaseAmount % LottoConstants.LOTTO_PRICE != LottoConstants.NO_REMAINDER)
+            return true;
+        return false;
     }
 }
---
+++ src/main/java/lotto/LottoConstants.java
@@ -0,0 +1,16 @@
+package lotto;
+
+public class LottoConstants {
+    private LottoConstants() {
+    };
+
+    public static final boolean OCCURRENCE_ERROR = true;
+
+    public static final int LOTTO_PRICE = 1_000;
+    public static final int NO_REMAINDER = 0;
+
+    public static final String INPUT_PURCHASE_AMOUNT_GUIDE_MESSAGE = "구입금액을 입력해 주세요.";
+
+    public static final String MULTIPLE_OF_UNIT_ERROR_MESSAGE = "[ERROR] 1,000원 단위로 입력해야 합니다.";
+    public static final String NOT_POSITIVE_INTEGER_ERROR_MESSAGE = "[ERROR] 입력은 양의 정수여야 합니다.";
+}
